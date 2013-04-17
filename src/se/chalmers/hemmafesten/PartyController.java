3dd3cd580b8553caec542a1af9f340d456f7cefb
kloek <anton_p90@hotmail.com>
2013-04-17 14:07:01 +0200

    added get list and singelton to partycontroller.
    Signed-off-by: kloek <anton_p90@hotmail.com>

--- src/se/chalmers/hemmafesten/PartyController.java
+++ src/se/chalmers/hemmafesten/PartyController.java
@@ -2,8 +2,11 @@ package se.chalmers.hemmafesten;
 
 
 
+import java.util.List;
+
 import android.util.Log;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -12,11 +15,36 @@ import com.parse.ParseRelation;
 
 public class PartyController {
 	
+	private static PartyController instance = null;
+	
 	private boolean isCreator;
 	private ParseObject party = null;
 	private String accessCode;
 
-	public PartyController() {  // creating a new party
+	
+	public PartyController getInstance(){
+		return instance;
+	}
+	
+	public void createParty(){
+		if(instance == null){
+			instance = new PartyController();
+		}
+	}
+	
+	public void joinParty(String accessCode){
+		if(instance == null){
+			instance = new PartyController(accessCode);
+		}else{
+			// add code for telling you that you'r already connected to a party?
+		}
+		
+	}
+	
+	/**
+	 * Construct for creating a new party
+	 */
+	private PartyController() {  // creating a new party
 		this.isCreator = true;
 		party = new ParseObject("Party");
 		party.saveInBackground();
@@ -24,10 +52,10 @@ public class PartyController {
 	
 	
 	/**
-	 * 
+	 * construct for creating a partyController connected to an existing party
 	 * @param accessCode
 	 */
-	public PartyController(String accessCode) {  // joining an existing party
+	private PartyController(String accessCode) {  // joining an existing party
 		ParseQuery query = new ParseQuery("party");
 		query.getInBackground(accessCode, new GetCallback() {
 		  public void done(ParseObject object, ParseException e) {
@@ -51,6 +79,7 @@ public class PartyController {
 		if(party != null){
 			try {
 				ParseObject song = new ParseObject("Song");
+				song.put("party", party);
 				song.put("spotifyURI", spotifyURI);
 				song.save();
 				
@@ -90,6 +119,18 @@ public class PartyController {
 
 	public ParseObject getParty() {
 		return party;
+	}
+	
+	public List<ParseObject> getList(){
+		try {
+			ParseQuery query = new ParseQuery("Song");
+			query.whereEqualTo("party", party);
+			return query.find();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+		
 	}
 
 }
