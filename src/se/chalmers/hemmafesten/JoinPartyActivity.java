1072eb67e2c341767db9611f914809c560a2d14f
kioksy <ante89@gmail.com>
2013-04-19 15:07:53 +0200

    ante

--- src/se/chalmers/hemmafesten/JoinPartyActivity.java
+++ src/se/chalmers/hemmafesten/JoinPartyActivity.java
@@ -76,9 +76,10 @@ public class JoinPartyActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.join_party, menu);
-		return true;
+	//	// Inflate the menu; this adds items to the action bar if it is present.
+	//	getMenuInflater().inflate(R.menu.join_party, menu);
+	//	return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
