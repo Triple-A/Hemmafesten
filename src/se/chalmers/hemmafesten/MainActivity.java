a7d828081fef7e2ca3db47730c01e0c955eecb55
kloek <anton_p90@hotmail.com>
2013-04-22 14:54:18 +0200

    messagehandler

--- src/se/chalmers/hemmafesten/MainActivity.java
+++ src/se/chalmers/hemmafesten/MainActivity.java
@@ -2,40 +2,41 @@ package se.chalmers.hemmafesten;
 
 import se.chalmers.hemmafesten.PartyService.Status;
 
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
     
-    
-    public void createParty(View sender) {
+    private boolean pcIsBound;
+    Messenger mService = null;
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+	public void clickCreateParty(View sender) {
     	createPartyService(true);
     }
     
     
-    public void joinParty(View sender) {
+	public void clickJoinParty(View sender) {
     	View joinFrame = findViewById(R.id.joinFrame);
-    	
-    	int visible = joinFrame.getVisibility();
-    	
-    	if(visible == 0){ //visible
+    	if(joinFrame.getVisibility() == 0){ //visible
     		joinFrame.setVisibility(8);
-    	}else if(visible == 4){ // invisble
-    		joinFrame.setVisibility(0);
-    	}else{// 2 = gone
+    	}else{// 8 = gone
     		joinFrame.setVisibility(0);
     	}
-    	
     }
     
     
@@ -52,12 +53,56 @@ public class MainActivity extends Activity {
     	if(PartyService.getStatus() == Status.FREE){
 			Intent partyIntent = new Intent(this, PartyService.class); //                          
 			partyIntent.putExtra("isCreator", isCreator);                  //
-			if(!isCreator) partyIntent.putExtra("accessCode", getCodeInput());
+			if(!isCreator){
+				partyIntent.putExtra("accessCode", "MqF3jjzsw0"); //getCodeInput()
+			}
 			startService(partyIntent);                                 // Starting partyService
-		}else{
+    	}else{
 				//PartyService busy
 		}
+    	doBindService();
     }
+    
+    
+    void doBindService() {
+        bindService(new Intent(this, PartyService.class), mConnection, Context.BIND_AUTO_CREATE);
+        pcIsBound = true;
+    }
+    void doUnbindService() {
+        if (pcIsBound) {
+            // If we have received the service, and hence registered with it, then now is the time to unregister.
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null, PartyService.MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                } catch (RemoteException e) {
+                    // There is nothing special we need to do if the service has crashed.
+                }
+            }
+            // Detach our existing connection.
+            unbindService(mConnection);
+            pcIsBound = false;
+        }
+    }
+    
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mService = new Messenger(service);
+            try {
+                Message msg = Message.obtain(null, PartyService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+            } catch (RemoteException e) {
+                Log.e("MainActivity", "onServiceConnecte: " + e.getMessage());
+            }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been unexpectedly disconnected - process crashed.
+            mService = null;
+        }
+    };
     
     private String getCodeInput(){
 		EditText et = (EditText)findViewById(R.id.accessInput);  // accessCode input
@@ -65,8 +110,24 @@ public class MainActivity extends Activity {
 	}
     
     
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+        	switch(PartyService.getStatus()){
+        		case FREE:
+        			break;
+        		case HOST:
+        		case GUEST:
+        			Intent intent = new Intent(null, PartyActivity.class);
+        			startActivity(intent);
+        			break;
+        		case FAILED:
+        			break;
+        	}
+        }
+    }
     
-    ///////////////////////////////////////////////////////////////////////////////////////
+    
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- src/se/chalmers/hemmafesten/PartyService.java
+++ src/se/chalmers/hemmafesten/PartyService.java
@@ -1,12 +1,20 @@
 package se.chalmers.hemmafesten;
 
 
+import java.util.ArrayList;
+
 import com.parse.Parse;
 
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
 
 
 public class PartyService extends Service {
@@ -19,13 +27,19 @@ public class PartyService extends Service {
 	    GUEST,
 	    FAILED
 	}
+	static final int MSG_REGISTER_CLIENT = 1;
+    static final int MSG_UNREGISTER_CLIENT = 2;
+	
+	private ArrayList<Messenger> mClients = new ArrayList<Messenger>(); // Keeps track of all current registered clients.
+	final Messenger mMessenger = new Messenger(new IncomingHandler()); // Target we publish for clients to send messages to IncomingHandler.
+	/** For showing and hiding our notification. */
+    private NotificationManager mNM;
 
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		
 		if(status == Status.FREE){
-			Parse.initialize(this, APIKeys.ParseApplicationID(), APIKeys.ParseClientKey());
 			initiateParty(intent);
 		}
 	    return Service.START_NOT_STICKY;
@@ -43,7 +57,34 @@ public class PartyService extends Service {
 			pc = new PartyController(accessCode);
 			status = ((pc != null) ? Status.GUEST : Status.FAILED);
 		}
+		sendMessageToUI();
 	}
+	
+	private void sendMessageToUI() {
+		for (int i=0; i < mClients.size(); i++) {
+            try {
+                mClients.get(i).send(null);
+            } catch (RemoteException e) {
+                mClients.remove(i);
+            }
+        }
+    }
+	
+	class IncomingHandler extends Handler { // Handler of incoming messages from clients.
+        @Override
+        public void handleMessage(Message msg) {
+        	switch (msg.what) {
+            case MSG_REGISTER_CLIENT:
+                mClients.add(msg.replyTo);
+                break;
+            case MSG_UNREGISTER_CLIENT:
+                mClients.remove(msg.replyTo);
+                break;
+            
+        	}
+        }
+	}
+	
 	
 	public void addSong(String spotifyURI){
 		pc.addSong(spotifyURI);
@@ -57,12 +98,22 @@ public class PartyService extends Service {
 		return pc;
 	}
 	
-	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public void killService(){
+		pc.killParty(status == Status.HOST);
+		status = Status.FREE;
+		stopSelf();
 	}
-
-
-
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mMessenger.getBinder();
+	}
+	
+	@Override
+	public void onCreate() {
+	        super.onCreate();
+	        Parse.initialize(this, APIKeys.ParseApplicationID(), APIKeys.ParseClientKey());
+	        Log.i("PartyService", "Service Started.");
+	}
 }
+
