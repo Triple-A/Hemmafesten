3d9f8ea524aa12fa015d2ae647b443139888afe3
kioksy <ante89@gmail.com>
2013-04-24 19:15:47 +0200

    hehe

---
+++ src/se/chalmers/hemmafesten/ActionBarActivity.java
@@ -0,0 +1,69 @@
+package se.chalmers.hemmafesten;
+
+import java.util.zip.Inflater;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+public class ActionBarActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_action_bar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+	    //set up search view
+
+	    inflater.inflate(R.menu.action_bar, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId,MenuItem item) {
+	    switch (item.getItemId()) {
+	        case R.id.home:
+	        	Intent homeIntent = new Intent(this, MainActivity.class);
+	        	homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+	        	startActivity(homeIntent);
+	        	return true;
+	        case R.id.leaveParty:
+	            Intent leavePartyintent = new Intent(this, ActionBarActivity.class);
+	            leavePartyintent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+	            startActivity(leavePartyintent);
+	            return true;
+	        case R.id.savedParties:
+	            Intent savedPartiesIntent = new Intent(this, SavedPartiesActivity.class);
+	            savedPartiesIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+	            startActivity(savedPartiesIntent);
+	            return true;
+	        case R.id.help:
+	            Intent helpIntent = new Intent(this, HelpActivity.class);
+	            helpIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+	            startActivity(helpIntent);
+	            return true;
+	        case R.id.settings:
+	            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+	            settingsIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+	            startActivity(settingsIntent);
+	        	return true;
+	        default:
+	            return false;
+	    }
+	}
+
+	
+}
---
+++ src/se/chalmers/hemmafesten/SavedPartiesActivity.java
@@ -0,0 +1,21 @@
+package se.chalmers.hemmafesten;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SavedPartiesActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_saved_parties);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		return super.onCreateOptionsMenu(menu);
+	}
+
+}
