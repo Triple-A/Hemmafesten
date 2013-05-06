9326291fb0fd4fcf902d83bdfaa76f96c3a12fbd
Aron Cedercrantz <aron@cedercrantz.se>
2013-05-06 17:45:57 +0200

    Add a wrapper model class for a party.
    Signed-off-by: Aron Cedercrantz <aron@cedercrantz.se>

---
+++ src/se/chalmers/hemmafesten/model/Party.java
@@ -0,0 +1,85 @@
+package se.chalmers.hemmafesten.model;
+
+import com.parse.ParseObject;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+public class Party extends Model {
+	public Party() {
+		super(new ParseObject(getParseObjectName()));
+	}
+	
+	public Party(ParseObject parseObject) {
+		super(parseObject);
+	}
+	
+	
+	// Getters and setters
+	public String getName() {
+		return this.getParseObject().getString("name");
+	}
+	
+	public void setName(String name) {
+		this.getParseObject().put("name", name);
+	}
+	
+	public String getAccessCode() {
+		return this.getParseObject().getString("accessCode");
+	}
+	
+	public void setAccessCode(String accessCode) {
+		this.getParseObject().put("accessCode", accessCode);
+	}
+	
+	public ParseUser getHost() {
+		return this.getParseObject().getParseUser("host");
+	}
+	
+	public void setHost(ParseUser host) {
+		this.getParseObject().put("host", host);
+	}
+	
+	public ParseRelation attendees() {
+		return this.getParseObject().getRelation("attendees");
+	}
+	
+	public void addAttendee(ParseUser user) {
+		this.attendees().add(user);
+	}
+	
+	public void removeAttendee(ParseUser user) {
+		this.attendees().remove(user);
+	}
+	
+	public ParseRelation songs() {
+		return this.getParseObject().getRelation("songs");
+	}
+	
+	public void addSong(Song song) {
+		ParseObject parseSongObject = song.getParseObject();
+		this.songs().add(parseSongObject);
+	}
+	
+	public void removeSong(Song song) {
+		ParseObject parseSongObject = song.getParseObject();
+		this.songs().remove(parseSongObject);
+	}
+	
+	// Description
+	@Override
+	public String toString() {
+		return "" + this.getClass().getCanonicalName() + " { identifier = '"+ this.getParseObject().getObjectId() +"'; }"; 
+	}
+	
+	
+	// Private helpers
+	private static String getParseObjectName() {
+		return "Song";
+	}
+	
+	@Override
+	protected String getParseObjectNameInternal() {
+		return getParseObjectName();
+	}
+
+}
