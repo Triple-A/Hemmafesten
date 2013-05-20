aecb488e8884c37061454b3f57c648ee9307550b
Aron Cedercrantz <aron@cedercrantz.se>
2013-05-20 17:39:18 +0200

    Change so we use the defined constants and not explicit values.
    Signed-off-by: Aron Cedercrantz <aron@cedercrantz.se>

--- src/se/chalmers/hemmafesten/activity/MainActivity.java
+++ src/se/chalmers/hemmafesten/activity/MainActivity.java
@@ -168,7 +168,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onResume(){
     	super.onResume();
     	doBindService();
-    	joinFrame.setVisibility(8);
+    	joinFrame.setVisibility(View.GONE);
     	activePartyVisibility();
     }
     
@@ -186,9 +186,9 @@ public class MainActivity extends ActionBarActivity {
     
     private void activePartyVisibility(){
     	if(PartyService.getStatus() == Status.GUEST || PartyService.getStatus() == Status.HOST){
-    		activePartyFrame.setVisibility(0);
+    		activePartyFrame.setVisibility(View.VISIBLE);
     	}else{
-    		activePartyFrame.setVisibility(8);
+    		activePartyFrame.setVisibility(View.GONE);
     	}
     }
 
