c5488276add862bce642cb7a09bc031be51d7b36
kioksy <ante89@gmail.com>
2013-05-20 12:21:03 +0200

    hej

--- src/se/chalmers/hemmafesten/activity/SavedPartiesActivity.java
+++ src/se/chalmers/hemmafesten/activity/SavedPartiesActivity.java
@@ -14,7 +14,6 @@ public class SavedPartiesActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		return super.onCreateOptionsMenu(menu);
 	}
 
